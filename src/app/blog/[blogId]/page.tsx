209bc0a698347e4482001ae56c682ed10f7b3cb5
mohammad <mohammadalirezaie081@gmail.com>
2025-02-10 19:53:17 +0330

    create singleBlog page

--- src/app/blog/[blogId]/page.tsx
+++ src/app/blog/[blogId]/page.tsx
@@ -1,7 +1,24 @@
+import SingleBlogContent from "@/components/blog/SingleBlogContent";
+import Image from "next/image";
 import React from "react";
 
 const blogPage = () => {
-  return <div>blogPage</div>;
+  return (
+    <div className="container min-h-[100dvh] bg-zinc-100 flex flex-col items-center justify-start py-4">
+      <div className="relative w-[80%] h-[50dvh] ">
+        <Image
+          src="/banner-1.jpg"
+          fill
+          className="object-cover"
+          alt=""
+          sizes="fill"
+        />
+      </div>
+
+      {/* Text contents */}
+      <SingleBlogContent />
+    </div>
+  );
 };
 
 export default blogPage;
